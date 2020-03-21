5a133cffcf2620ac55a1fdc421ff6d2f2ab1d859
non555963 <nontawat555963@gmail.com>
2020-03-21 17:55:54 +0700

    add git test

---
+++ test1.js
@@ -0,0 +1,9 @@
+const name = 'Non';
+const hello = name => `Hello ${name}`;
+
+// const sum = (a, b) => a + b;
+// const result = sum(2, 3);
+// console.log('Result : ',result); 
+
+// module.exports = sum;
+hello();
