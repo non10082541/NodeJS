322845c17ec7ccd5def034677a250bfb91d5231f
non555963 <nontawat555963@gmail.com>
2020-03-22 22:23:50 +0700

    add readFile

--- readFile.js
+++ readFile.js
@@ -1,11 +1,11 @@
 const fs = require('fs');
 
-fs.readFile('C:/Nontawat/FreeTime/data.txt','utf-8', (error, data) => {
+fs.readFile('E:/Code/data.txt','utf-8', (error, data) => {
 // fs.readFile('data.txt', (error, data) => {
     if (error){
         console.log('error');
     }
-    window.Showdata = data;
+   console.log('data : ',data);
 });
 
 // console.log('data : ',data);
